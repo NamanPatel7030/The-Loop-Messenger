03e9e965dabe991787733b36aa6d634778181fb6
Naman Developer <138224004+NamanPatel7030@users.noreply.github.com>
2024-11-26 21:25:25 +0530

    Update chat_gpt_service.dart

--- lib/service/chat_gpt_service.dart
+++ lib/service/chat_gpt_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ChatGPTService {
-  final String apiKey = "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"; // Replace with your actual API key
+  final String apiKey = ""; // Replace with your actual API key
 
   Future<String> sendMessage(String message) async {
     const String url = "https://api.openai.com/v1/chat/completions";
